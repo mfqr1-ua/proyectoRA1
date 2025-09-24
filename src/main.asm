f02f3d405eaa2edac0766b1ef489262a8b668624
mfqr1-ua <98946818+mfqr1-ua@users.noreply.github.com>
2025-09-24 16:15:25 +0200

    inicio
    comienzo de proyecto, conseguimos movimiento en el eje x

---
+++ src/main.asm
@@ -0,0 +1,55 @@
+SECTION "Entry point", ROM0[$150]
+
+wait_vblank:
+    ld  a, [$FF44]          
+    cp   144
+    jr   c, wait_vblank
+    ret
+
+read_dpad:
+    ld   a, $20            ; seleccionar dpad
+    ld  [$FF00], a          
+    ld  a, [$FF00]
+    ld  a, [$FF00]          ; estabiliza
+    ret
+
+main::
+    ld   hl, $9A09
+    ld   a,  $19
+    ld   [hl], a
+
+.loop:
+    call read_dpad
+
+    bit  0, a
+    jr   nz, .comprobarIzquierda         ; si no, miramos izquierda
+
+    call wait_vblank
+    xor  b                       
+    ld   [hl], b                 ; borra casilla actual
+    inc  hl                      ; avanza a la derecha
+    ld   a, $19
+    ld   [hl], a                 
+
+.esperaRigth:              
+    call read_dpad
+    bit  0, a
+    jr   z, .esperaRigth
+    jr   .loop
+
+.comprobarIzquierda:
+    bit  1, a
+    jr   nz, .loop               ; si tampoco, no hacemos nada
+
+    call wait_vblank
+    xor  b                       
+    ld   [hl], b                 ; borra casilla actual
+    dec  hl                      ; retrocede a la izquierda
+    ld   a, $19
+    ld   [hl], a
+
+.esperaLeft:               
+    call read_dpad
+    bit  1, a 
+    jr   z, .esperaLeft
+    jr   .loop
