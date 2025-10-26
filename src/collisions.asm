16e3f3b159436b7f1b9c401cc4183545906eeb87
alejadrobeply <alejandro@beply.es>
2025-10-26 03:23:52 +0100

    prueba 3

--- src/collisions.asm
+++ src/collisions.asm
@@ -19,26 +19,35 @@ revisar_colisiones_balas_enemigos:
     ; Verificar si el slot tiene una bala activa
     ld   h, $C0
     ld   l, c
-    ld   a, [hl]                 ; Leer X de la bala
-    or   a
-    jr   z, .next_bala           ; Slot vacío
     
-    ; Verificar que NO sea enemigo (las balas no tienen ATTR_ENEMIGO)
+    ; Verificar ATTR primero para saber si es bala o enemigo
     push hl
     inc  hl
     inc  hl
     inc  hl
     ld   a, [hl]
-    cp   ATTR_ENEMIGO
     pop  hl
+    cp   ATTR_ENEMIGO
     jr   z, .next_bala           ; Es enemigo, no bala
     
-    ; Tenemos una bala en HL, guardar su slot
-    push bc
-    call check_bala_vs_all_enemies
-    pop  bc
+    ; Ahora verificar si está activa
+    ld   a, [hl]                 ; Leer X de la bala
+    or   a
+    jr   z, .next_bala           ; Slot vacío
     
-    ; Si hubo colisión, la bala ya fue eliminada
+    ; Tenemos una bala activa, guardar posición
+    ld   e, a                    ; E = bala_x
+    inc  hl
+    ld   a, [hl]
+    ld   d, a                    ; D = bala_y
+    dec  hl
+    
+    ; E = bala_x, D = bala_y, C = slot de bala
+    push bc
+    push de
+    call check_bala_vs_all_enemies
+    pop  de
+    pop  bc
     
 .next_bala:
     ld   a, c
@@ -48,26 +57,14 @@ revisar_colisiones_balas_enemigos:
 
 ; ------------------------------------------------------------
 ; check_bala_vs_all_enemies
-; HL apunta al slot de la bala
-; C contiene el slot de la bala
+; E = bala_x, D = bala_y, C = slot de bala
 ; ------------------------------------------------------------
 check_bala_vs_all_enemies:
-    ; Guardar posición de la bala
-    ld   a, [hl+]                ; X de bala
-    ld   d, a
-    ld   a, [hl]                 ; Y de bala
-    ld   e, a
-    
-    ; D = bala_x, E = bala_y, C = slot de bala
-    
-    ; Recorrer enemigos
-    push bc
-    push de
     ld   b, 4                    ; Primer slot de enemigos
 .loop_enemies:
     ld   a, b
     cp   40
-    jr   z, .no_collision
+    ret  z                       ; No más enemigos
     
     ; Verificar si es enemigo
     ld   h, $C0
@@ -78,8 +75,8 @@ check_bala_vs_all_enemies:
     inc  hl
     inc  hl
     ld   a, [hl]
-    cp   ATTR_ENEMIGO
     pop  hl
+    cp   ATTR_ENEMIGO
     jr   nz, .next_enemy
     
     ; Es enemigo, verificar si está activo
@@ -87,109 +84,68 @@ check_bala_vs_all_enemies:
     or   a
     jr   z, .next_enemy
     
-    ; Tenemos enemigo activo, verificar colisión
+    ; Enemigo activo, leer posición
     push bc
-    pop  de
-    pop  af                      ; Recuperar bala_y en A
-    pop  bc                      ; Recuperar bala_x en B
-    push bc
-    push af
     push de
     
-    ; B = bala_x, A = bala_y (en stack)
-    ; HL apunta a enemigo
-    ; E = slot de enemigo
+    ld   a, [hl+]                ; enemy_x
+    ld   h, a
+    ld   a, [hl]                 ; enemy_y
+    ld   l, a
     
-    call check_collision_bala_vs_enemy_slot
-    jr   nz, .collision_found
+    ; H = enemy_x, L = enemy_y
+    ; D = bala_y, E = bala_x (en stack)
+    
+    pop  de                      ; E = bala_x, D = bala_y
+    
+    ; Verificar colisión simple: bala_x == enemy_x ± 2 && bala_y == enemy_y ± 2
+    ld   a, e                    ; bala_x
+    sub  h                       ; bala_x - enemy_x
+    jr   nc, .diff_x_positive
+    ; Negativo
+    cpl
+    inc  a
+.diff_x_positive:
+    cp   4                       ; Distancia X < 4
+    jr   nc, .no_collision
+    
+    ld   a, d                    ; bala_y
+    sub  l                       ; bala_y - enemy_y
+    jr   nc, .diff_y_positive
+    ; Negativo
+    cpl
+    inc  a
+.diff_y_positive:
+    cp   3                       ; Distancia Y < 3
+    jr   nc, .no_collision
+    
+    ; ¡Colisión detectada!
+    pop  bc                      ; B = slot de enemigo
+    
+    ; Guardar slot de bala en stack
+    push de
+    
+    ; Eliminar enemigo (B contiene el slot)
+    ld   c, b
+    call eliminar_enemigo_por_slot
+    
+    ; Eliminar bala (C del exterior contiene el slot)
+    pop  de
+    pop  bc
+    push bc
+    call eliminar_bala_actual
+    pop  bc
+    
+    ret                          ; Salir después de la colisión
+    
+.no_collision:
+    pop  bc
     
 .next_enemy:
     ld   a, b
     add  a, 4
     ld   b, a
     jr   .loop_enemies
-    
-.no_collision:
-    pop  de
-    pop  bc
-    ret
-
-.collision_found:
-    ; Colisión detectada!
-    ; E = slot de enemigo
-    ; C (en stack) = slot de bala
-    
-    pop  de                      ; Limpia stack (bala pos)
-    pop  bc                      ; BC = (bala_x, bala_y) - no usado
-    pop  bc                      ; C = slot de bala
-    
-    push bc
-    push de
-    
-    ; Eliminar enemigo (E contiene el slot)
-    ld   c, e
-    call eliminar_enemigo_por_slot
-    
-    ; Eliminar bala
-    pop  de
-    pop  bc
-    call eliminar_bala_actual
-    
-    ret
-
-; ------------------------------------------------------------
-; check_collision_bala_vs_enemy_slot
-; Verifica colisión AABB entre bala y enemigo
-; HL apunta al enemigo
-; B = bala_x, A = bala_y (en stack top), E = slot enemigo
-; Salida: Z flag clear si hay colisión
-; ------------------------------------------------------------
-check_collision_bala_vs_enemy_slot:
-    ; Leer posición del enemigo
-    ld   a, [hl+]                ; enemy_x
-    ld   c, a
-    ld   a, [hl]                 ; enemy_y
-    ld   d, a
-    
-    ; C = enemy_x, D = enemy_y
-    ; B = bala_x, stack top = bala_y
-    
-    ; Verificar X: |bala_x - enemy_x| < umbral
-    ld   a, b
-    sub  c
-    jr   nc, .positive_x
-    ; Si es negativo, negar manualmente: A = 0 - A
-    cpl
-    inc  a
-.positive_x:
-    add  a, 2                    ; Margen de colisión X
-    cp   5                       ; Ancho de detección (3x2 tiles)
-    jr   nc, .no_hit
-    
-    ; Verificar Y
-    pop  hl                      ; Recuperar dirección de retorno
-    pop  af                      ; A = bala_y
-    push af
-    push hl
-    
-    sub  d
-    jr   nc, .positive_y
-    ; Si es negativo, negar manualmente: A = 0 - A
-    cpl
-    inc  a
-.positive_y:
-    add  a, 5                    ; Margen de colisión Y
-    cp   10                      ; Alto de detección
-    jr   nc, .no_hit
-    
-    ; Colisión!
-    xor  a
-    inc  a                       ; Clear Z flag
-    ret
-    
-.no_hit:
-    xor  a                       ; Set Z flag
-    ret
 
 ; ------------------------------------------------------------
 ; eliminar_bala_actual
@@ -215,6 +171,7 @@ eliminar_bala_actual:
     ; Borrar del mapa
     push bc
     call calcular_direccion_bg_desde_xy
+    call wait_vblank
     xor  a
     ld   [hl], a                 ; Tile vacío
     pop  bc
